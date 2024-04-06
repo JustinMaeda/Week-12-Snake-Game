f2346251d418bdfee97691e8806a3e45f423de1e
JustinMaeda <jkmaeda@hawaii.edu>
2024-04-06 13:37:33 -1000

    Create Snake

--- Snake.py
+++ Snake.py
@@ -12,8 +12,22 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Snake')
 
+
+#define game variables
+cell_size = 10
+
+#create snake
+snake_pos = [[int(screen_width / 2), int(screen_height / 2)]]
+snake_pos.append([int(screen_width / 2), int(screen_height / 2) + cell_size])
+snake_pos.append([int(screen_width / 2), int(screen_height / 2) + cell_size * 2])
+snake_pos.append([int(screen_width / 2), int(screen_height / 2) + cell_size * 3])
+
+
 #define background colors
 bg = (255, 200, 150)
+body_inner = (50, 175, 25)
+body_outer = (100, 100, 200)
+
 
 def draw_screen():
     screen.fill(bg)
@@ -21,6 +35,8 @@ def draw_screen():
 #setup loop with exit
 run = True
 while run:
+
+    draw_screen()
 
     #iterate
     for event in pygame.event.get():
