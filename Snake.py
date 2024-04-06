21c9055440991dcb81c3a66818e997187bb96207
JustinMaeda <jkmaeda@hawaii.edu>
2024-04-06 13:02:20 -1000

    Create Game Loop

--- Snake.py
+++ Snake.py
@@ -13,3 +13,19 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Snake')
 
 
+#setup loop with exit
+run = True
+while run:
+
+    #iterate
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+    #update display
+    pygame.display.update()
+
+
+#end game
+pygame.quit()
