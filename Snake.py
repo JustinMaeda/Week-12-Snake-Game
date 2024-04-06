77c40ee3aab09e865a3af1210b137207491015a4
JustinMaeda <jkmaeda@hawaii.edu>
2024-04-06 12:57:42 -1000

    Create Game Window with pygame

--- Snake.py
+++ Snake.py
@@ -4,3 +4,12 @@ from pygame.locals import *
 
 pygame.init()
 
+#create blank game window
+screen_width = 600
+screen_height = 600
+
+#create game window
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Snake')
+
+
