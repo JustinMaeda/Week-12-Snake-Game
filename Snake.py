03695a3fea846a048c2377acb5d5741c31625a6e
JustinMaeda <jkmaeda@hawaii.edu>
2024-04-06 13:42:56 -1000

    Draw Snake

--- Snake.py
+++ Snake.py
@@ -1,29 +1,27 @@
-#import modules
+# import modules
 import pygame
 from pygame.locals import *
 
 pygame.init()
 
-#create blank game window
+# create blank game window
 screen_width = 600
 screen_height = 600
 
-#create game window
+# create game window
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Snake')
 
-
-#define game variables
+# define game variables
 cell_size = 10
 
-#create snake
-snake_pos = [[int(screen_width / 2), int(screen_height / 2)]]
-snake_pos.append([int(screen_width / 2), int(screen_height / 2) + cell_size])
-snake_pos.append([int(screen_width / 2), int(screen_height / 2) + cell_size * 2])
-snake_pos.append([int(screen_width / 2), int(screen_height / 2) + cell_size * 3])
+# create snake
+snake_pos = [[int(screen_width / 2), int(screen_height / 2)],
+             [int(screen_width / 2), int(screen_height / 2) + cell_size],
+             [int(screen_width / 2), int(screen_height / 2) + cell_size * 2],
+             [int(screen_width / 2), int(screen_height / 2) + cell_size * 3]]
 
-
-#define background colors
+# define background colors
 bg = (255, 200, 150)
 body_inner = (50, 175, 25)
 body_outer = (100, 100, 200)
@@ -32,21 +30,24 @@ body_outer = (100, 100, 200)
 def draw_screen():
     screen.fill(bg)
 
-#setup loop with exit
+
+# setup loop with exit
 run = True
 while run:
 
     draw_screen()
 
-    #iterate
+    # iterate
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-
-    #update display
+    # draw snake
+    for x in snake_pos:
+        pygame.draw.rect(screen, body_outer, (x[0], x[1], cell_size, cell_size))
+        pygame.draw.rect(screen, body_inner, (x[0] + 1, x[1] + 1, cell_size - 2, cell_size - 2))
+    # update display
     pygame.display.update()
 
-
-#end game
+# end game
 pygame.quit()
